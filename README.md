4403532f8ac85fa043881ccbae072afc097639cc
unknown <LucasMacchi>
2023-01-31 19:36:21 -0300

    addign

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # MovieRating_Back
 Backend of Movie reviews app
+
+https://rapidapi.com/apidojo/api/flixster/
+
+Routes:
+
+Movie Routes:
+    - /:name --> this will bring movies matching the name searched
--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@ import morgan from "morgan"
 import bodyParser from "body-parser"
 import * as dotenv from "dotenv"
 dotenv.config()
-
+import { router } from "./Routes"
 // Create server
 
 export const server = express()
@@ -14,7 +14,7 @@ export const PORT = process.env.PORT
 server.use(morgan("short"))
 server.use(bodyParser.urlencoded({extended:true, limit: "10mb"}))
 server.use(bodyParser.json({limit: "10mb"}))
-
+server.use("/", router)
 //test
 server.get("/ping", (_req, res) => {
     const message: string = "Server is running at port "+PORT +", ping recived at "+ new Date().toLocaleDateString()
