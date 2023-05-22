96bc48cc42d93b30c84718e958f9b7b51d53dd55
LucasMacchi <lucasmacchi25@gmail.com>
2023-05-22 19:48:30 -0300

    deleteing tokens now deletes all the user tokens

--- Routes/Controllers/deleteTokenPassword.ts
+++ Routes/Controllers/deleteTokenPassword.ts
@@ -3,6 +3,7 @@ import connexion from "../.."
 export default async function (id: string){
     const token = await connexion.models.Token.findByPk(id)
     if(!token) throw Error("Token doesn't exits" )
-    await token.destroy()
+    const allTokens = await connexion.models.Token.findAll({where:{user_id: token.dataValues.user_id}})
+    allTokens.map(async (t) => await t.destroy())
     return "Token deleted"
 }
